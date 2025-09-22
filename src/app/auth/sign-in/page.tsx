2812b7b14a2e05d3f85cffbde8e326edd3a060cf
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-22 14:52:35 +0300

    style: add animation on sign block

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -2,16 +2,23 @@ import { OAuth } from "@/components/ui/Auth/OAuth";
 import { SignIn } from "@/components/ui/Auth/SignIn";
 import { ModeToggle } from "@/components/ui/Button/DarkModeToogle";
 import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
+import * as motion from "motion/react-client";
 
 const Page = () => {
   return (
-    <div className="flex flex-col gap-5 p-5  hover:shadow-2xl dark:hover:shadow-blue-900 transition-all duration-200 ease-in-out rounded-3xl justify-center ">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 0.4,
+      }}
+      className="flex flex-col gap-5 p-5  hover:shadow-2xl dark:hover:shadow-blue-900 transition-all duration-200 ease-in-out rounded-3xl justify-center "
+    >
       <ModeToggle />
       <SignIn />
       <Separator />
       <OAuth />
-    </div>
+    </motion.div>
   );
 };
 
