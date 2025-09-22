e86a4c5200f73f586daf20690d66aa21c736d689
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-22 14:25:16 +0300

    style: add link to navigate between signup and in

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,14 +1,13 @@
 import { OAuth } from "@/components/ui/Auth/OAuth";
-import { SignIn } from "@/components/ui/Auth/SignIn";
+import { SignUp } from "@/components/ui/Auth/SignUp";
 import { ModeToggle } from "@/components/ui/Button/DarkModeToogle";
 import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
 
 const Page = () => {
   return (
     <div className="flex flex-col gap-5 p-5  hover:shadow-2xl dark:hover:shadow-blue-900 transition-all duration-200 ease-in-out rounded-3xl justify-center ">
       <ModeToggle />
-      <SignIn />
+      <SignUp />
       <Separator />
       <OAuth />
     </div>
--- src/components/ui/Auth/SignUp.tsx
+++ src/components/ui/Auth/SignUp.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { Button } from "../button";
 import { Input } from "../input";
 import { Label } from "../label";
-
-export const SignIn = () => {
+import PasswordStrengthBar from "react-password-strength-bar";
+export const SignUp = () => {
   return (
     <form className="flex flex-col justify-center items-center gap-5">
       <div className="flex flex-col gap-2">
@@ -13,13 +13,21 @@ export const SignIn = () => {
       </div>
       <div className="flex md:flex-row flex-col gap-4">
         <div className="flex flex-col gap-2">
-          <Label htmlFor="password">Your password</Label>
+          <Label htmlFor="NewPassword">New password</Label>
+          <Input type="password" id="NewPassword" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="password">Retype your password</Label>
           <Input type="password" id="password" />
         </div>
       </div>
+      <PasswordStrengthBar
+        shortScoreWord="Too short"
+        scoreWords={["Weak", "Fair", "Good", "Strong", "Very strong"]}
+      />
       <Button className="w-full">Sign in</Button>
-      <Link href="/auth/sign-up" className="text-sm  hover:underline">
-        I don't have an account.
+      <Link href="/auth/sign-in" className=" text-sm hover:underline">
+        I already have an account.
       </Link>
     </form>
   );
