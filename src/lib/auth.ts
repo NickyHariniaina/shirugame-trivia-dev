0557c53ddb1d48d6369b7ef3fc041a407b3c9bd3
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-23 15:22:27 +0300

    feat: signup basic logic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,7 +9,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     minPasswordLength: 8,
-    maxPasswordLength: 128,
+    maxPasswordLength: 20,
   },
   socialProviders: {
     github: {
