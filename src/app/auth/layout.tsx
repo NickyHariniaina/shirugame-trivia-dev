497ad53911659d2c9b172a19ae5d53708df91611
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-24 12:19:26 +0300

    feat: add custom font

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,37 +1,32 @@
-"use client";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import { BsBrightnessHigh } from "react-icons/bs";
 import { RiErrorWarningLine } from "react-icons/ri";
-// TODO: Add head for SEO later
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html className="w-full h-full" lang="en" suppressHydrationWarning>
-      <head />
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="flex flex-row justify-center items-center h-screen gap-3">
-            {children}
-          </div>
-          <Toaster
-            toastOptions={{
-              success: {
-                icon: <BsBrightnessHigh />
-              },
-              error: {
-                icon : <RiErrorWarningLine />
-              },
-            }}
-          />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="flex flex-col justify-center items-center h-screen gap-3">
+        {children}
+        <h1 className="text-xl md:text-5xl  m-3">Shirugame</h1>
+      </div>
+      <Toaster
+        toastOptions={{
+          success: {
+            icon: <BsBrightnessHigh />,
+          },
+          error: {
+            icon: <RiErrorWarningLine />,
+          },
+        }}
+      />
+    </ThemeProvider>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,25 @@
-import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 // TODO: Add head for SEO later
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html className="w-full h-full" lang="en" suppressHydrationWarning>
-      <head />
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+    <html suppressHydrationWarning>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rock+3D&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 };
