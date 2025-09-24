b64ebb39c97fa56b948451e979c095b8cb7b891e
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-24 10:18:31 +0300

    style: replace toast icon matching theme

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
-
+import { BsBrightnessHigh } from "react-icons/bs";
+import { RiErrorWarningLine } from "react-icons/ri";
 // TODO: Add head for SEO later
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -17,7 +19,16 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           <div className="flex flex-row justify-center items-center h-screen gap-3">
             {children}
           </div>
-          <Toaster />
+          <Toaster
+            toastOptions={{
+              success: {
+                icon: <BsBrightnessHigh />
+              },
+              error: {
+                icon : <RiErrorWarningLine />
+              },
+            }}
+          />
         </ThemeProvider>
       </body>
     </html>
