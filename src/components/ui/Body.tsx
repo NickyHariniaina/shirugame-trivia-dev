88902aca72ab7d93208bbbc2ef6f1df88a0e9c6b
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-26 17:59:00 +0300

    style: add button and desc text

--- src/components/ui/Body.tsx
+++ src/components/ui/Body.tsx
@@ -1,17 +1,23 @@
 import { motion } from "motion/react";
+import { Button } from "./button";
 
 type BodyPropsType = {
   logged: boolean;
 };
 export const Body = (props: BodyPropsType) => {
   return (
-    <div className="w-[100%] flex flex-col gap-2 justify-center items-center">
+    <div className="w-[100%] flex flex-col gap-4 justify-center items-center">
       <motion.h1
-        className="cursor-pointer text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl"
+        className="cursor-pointer text-4xl sm:text-5xl mb-2 md:text-6xl lg:text-7xl xl:text-8xl"
         whileHover={{ scale: 1.4 }}
       >
         Shirugame
       </motion.h1>
+      <p className="m-4 text-center">
+        Ready to use your brain with Shirugame ? Create a room, share the link,
+        and outsmart everyone else !
+      </p>
+      <Button variant="default">Create room</Button>
     </div>
   );
 };
