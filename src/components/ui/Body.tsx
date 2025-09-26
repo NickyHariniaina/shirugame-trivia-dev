fb74d9f2dbc12e21eeb6a36f7a5f2725d7188d2a
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-26 17:49:30 +0300

    style: add main title animation

--- src/components/ui/Body.tsx
+++ src/components/ui/Body.tsx
@@ -1,6 +1,17 @@
+import { motion } from "motion/react";
+
 type BodyPropsType = {
   logged: boolean;
 };
 export const Body = (props: BodyPropsType) => {
-  return <div>H</div>;
+  return (
+    <div className="w-[100%] flex flex-col gap-2 justify-center items-center">
+      <motion.h1
+        className="cursor-pointer text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl"
+        whileHover={{ scale: 1.4 }}
+      >
+        Shirugame
+      </motion.h1>
+    </div>
+  );
 };
