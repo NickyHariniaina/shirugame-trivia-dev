ee13029e52a8c9594774cddd2f974b26f3b0262d
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-29 11:48:30 +0300

    chore: I add useEffect for session"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,21 @@ import { Body } from "@/components/ui/Body";
 import { Footer } from "@/components/ui/Footer";
 import { Header } from "@/components/ui/Header";
 import { authClient } from "@/lib/auth-client";
+import { useState, useEffect } from "react";
 
 const Page = () => {
+  const [logged, setLogged] = useState<boolean>();
   const { data: session } = authClient.useSession();
   console.log(session);
+
+  useEffect(() => {
+    if (!session) {
+      setLogged(false);
+    } else {
+      setLogged(true);
+    }
+  }, [session]);
+
   return (
     <div className="flex flex-col m-2 gap-3">
       <Header logged={false} />
