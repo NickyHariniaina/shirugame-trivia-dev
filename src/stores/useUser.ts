47baf3c876504333c9bf70bb71179902dad7cf14
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-29 12:05:05 +0300

    chore: add store

---
+++ src/stores/useUser.ts
@@ -0,0 +1,26 @@
+import { Account, Session, User } from "@/types/db";
+import { create } from "zustand";
+
+type UserStore = {
+  userData: User | null;
+  userSession: Session | null;
+  userAccount: Account | null;
+  setUserData: (id: string) => void;
+  setUserSession: (id: string) => void;
+  setUserAccount: (id: string) => void;
+};
+
+export const useUser = create<UserStore>((set, get) => ({
+  userData: null,
+  userSession: null,
+  userAccount: null,
+  setUserData: async (id: string) => {
+    // TODO:
+  },
+  setUserAccount: async (id: string) => {
+    // TODO:
+  },
+  setUserSession: async (id: string) => {
+    // TODO:
+  },
+}));
