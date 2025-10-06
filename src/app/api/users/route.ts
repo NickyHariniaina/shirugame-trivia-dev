42ca576e59dec0b5114dda2a1a29f24c28982d06
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-06 10:37:27 +0300

    feat: add empty compenents

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,28 @@
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    const users = await prisma;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+};
