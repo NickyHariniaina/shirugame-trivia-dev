921f180a5365b0dc2eed1cc4396bfb18852d87db
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-18 14:09:00 +0300

    feat: add setUserData in the userStore

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
-import { Room, User } from "@/types/db";
+import {  User } from "@/types/db";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
--- src/stores/useUser.ts
+++ src/stores/useUser.ts
@@ -8,6 +8,7 @@ type UserStore = {
   fetchUserData: (id: string) => void;
   fetchUserSession: (id: string) => void;
   fetchUserAccount: (id: string) => void;
+  setUserData: (id: string, bodyReq: User) => void;
 };
 
 export const useUser = create<UserStore>((set, get) => ({
@@ -18,27 +19,41 @@ export const useUser = create<UserStore>((set, get) => ({
     const url = `/api/users/${id}`;
     const response = await fetch(url, {
       method: "GET",
-    })
+    });
     const data = await response.json();
     console.log(data.data);
-    set({ userData: data.data })
+    set({ userData: data.data });
   },
+
   fetchUserAccount: async (id: string) => {
     const url = `/api/users/${id}/account`;
     const response = await fetch(url, {
       method: "GET",
-    })
+    });
     const data = await response.json();
     console.log(data.data);
-    set({ userAccount: data.data })
+    set({ userAccount: data.data });
   },
+
   fetchUserSession: async (id: string) => {
     const url = `/api/users/${id}/session`;
     const response = await fetch(url, {
       method: "GET",
-    })
+    });
     const data = await response.json();
     console.log(data.data);
-    set({ userSession: data.data })
+    set({ userSession: data.data });
+  },
+
+  setUserData: async (id: string, bodyReq: User) => {
+    const url = `api/users/${id}`;
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(bodyReq),
+    });
+    get().fetchUserData(id);
   },
 }));
