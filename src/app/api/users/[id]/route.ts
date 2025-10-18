98378888f6ddff0a680364c1f57a341e72605bad
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-18 13:52:52 +0300

    feat: add user updater route

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,9 +1,13 @@
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { Room, User } from "@/types/db";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (_req: NextRequest, context: RouteContext<"/api/users/[id]"> ) => {
+export const GET = async (
+  _req: NextRequest,
+  context: RouteContext<"/api/users/[id]">,
+) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -13,8 +17,7 @@ export const GET = async (_req: NextRequest, context: RouteContext<"/api/users/[
   }
 
   try {
-
-    const userId = await context.params.id;
+    const { id: userId } = await context.params;
     const user = await prisma.user.findUnique({
       where: {
         id: userId,
@@ -38,4 +41,59 @@ export const GET = async (_req: NextRequest, context: RouteContext<"/api/users/[
       },
     );
   }
-}
+};
+
+export const POST = async (req: Request) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  try {
+    const dataReceived: User = await req.json();
+    await prisma.user.update({
+      where: {
+        id: session.user.id,
+      },
+      data: {
+        id: dataReceived.id,
+        name: dataReceived.name,
+        username: dataReceived.username,
+        rank: dataReceived.rank,
+        highestScore: dataReceived.highestScore,
+        email: dataReceived.email,
+        image: dataReceived.image,
+        updatedAt: dataReceived.updatedAt,
+      }
+    });
+
+    return NextResponse.json(
+      {
+        message: "User updated successfully.",
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+};
