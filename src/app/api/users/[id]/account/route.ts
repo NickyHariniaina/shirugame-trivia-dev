af455ecb313cfbe21b6caf9b57d5dc97ad8177a6
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-20 16:00:30 +0300

    fix: add context RouteContex into acconunt route

--- src/app/api/users/[id]/account/route.ts
+++ src/app/api/users/[id]/account/route.ts
@@ -1,9 +1,9 @@
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (_req: NextRequest, context: RouteContext<'/api/users/[id]'>) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
