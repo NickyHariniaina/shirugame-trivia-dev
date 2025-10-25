74f29f4d6f89a7585b779a223816fc032acbd4e2
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-25 16:13:34 +0300

    feat: add update user rank route

---
+++ src/app/api/users/[id]/rank/route.ts
@@ -0,0 +1,40 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { updateUserRank } from "@/services/user";
+
+export const PATCH = async (req: NextRequest, context: RouteContext<"/api/users/[id]/rank">) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+});
+
+if (!session) {
+  return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+}
+
+  try {
+    const { id: userId } = await context.params;
+    const dataReceived = await req.json();
+    const rank = dataReceived.rank;
+    await updateUserRank(userId, rank);
+
+    return NextResponse.json(
+      {
+        message: "User rank updated successfully.",
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
