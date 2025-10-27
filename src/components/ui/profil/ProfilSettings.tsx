99ae3c320174da4987ae90c8ecd3e3aa9c6be906
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-27 16:06:53 +0300

    feat: add better handling of errors in the settings page

--- src/components/ui/profil/ProfilSettings.tsx
+++ src/components/ui/profil/ProfilSettings.tsx
@@ -3,6 +3,7 @@ import { Input } from "../input";
 import { useUser } from "@/stores/useUser";
 import { Button } from "../button";
 import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 type ProfilSettingsPropsType = {
@@ -15,6 +16,8 @@ type FormValues = {
 };
 
 export const ProfilSettings = () => {
+  const [loading, setLoading] = useState(false);
+  let loadId: string | undefined;
   const { register, handleSubmit } = useForm<FormValues>();
   const { userData } = useUser();
   const saveData = async (data: FormValues) => {
@@ -28,27 +31,56 @@ export const ProfilSettings = () => {
 
   const handleChangeEmail = async (data: FormValues) => {
     if (data.email) {
-      await authClient.changeEmail({
+      setLoading(true);
+      loadId = toast.loading("Wait please...");
+
+      const { error } = await authClient.changeEmail({
         newEmail: data.email,
       });
+
+      if (error) {
+        toast.error(
+          error.message || "Something went wrong while changing the email",
+          { id: loadId },
+        );
+        setLoading(false);
+        return;
+      }
+
       toast.success("Email verification sent successfully", {
-        id: "email-sent",
+        id: loadId,
       });
+      setLoading(false);
     } else {
       toast.error("Email is required", { id: "email-required" });
+      setLoading(false);
     }
   };
 
   const handleChangeUsername = async (data: FormValues) => {
     if (data.username) {
       // TODO: format it in a better way
+      setLoading(true);
+      loadId = toast.loading("Wait please...");
       const verifiedUsername = data.username.replace(" ", "_");
-      await authClient.updateUser({ username: verifiedUsername });
-      toast.success("Username updated successfully", {
-        id: "username-updated",
+      const { error } = await authClient.updateUser({
+        username: verifiedUsername,
       });
+      if (error) {
+        toast.error(
+          error.message || "Something went wrong while updating the username",
+          { id: loadId },
+        );
+        setLoading(false);
+        return;
+      }
+      toast.success("Username updated successfully", {
+        id: loadId,
+      });
+      setLoading(false);
     } else {
       toast.error("Username is required", { id: "username-required" });
+      setLoading(false);
     }
   };
 
@@ -58,23 +90,36 @@ export const ProfilSettings = () => {
         <div className="flex flex-row gap-2 items-end">
           <div className="flex flex-col gap-2">
             <label htmlFor="username">Username</label>
-            <Input type="text" {...register("username")} placeholder={userData?.username} />
+            <Input
+              type="text"
+              {...register("username")}
+              placeholder={userData?.username}
+            />
           </div>
-          <Button onClick={handleSubmit(handleChangeUsername)}>
+          <Button
+            onClick={handleSubmit(handleChangeUsername)}
+            disabled={loading}
+          >
             Change
           </Button>
         </div>
         <div className="flex flex-row gap-2 items-end">
           <div className="flex flex-col gap-2">
             <label htmlFor="email">Email</label>
-            <Input type="email" placeholder={userData?.email} {...register("email")} />
+            <Input
+              type="email"
+              placeholder={userData?.email}
+              {...register("email")}
+            />
           </div>
-          <Button onClick={handleSubmit(handleChangeEmail)}>
+          <Button disabled={loading} onClick={handleSubmit(handleChangeEmail)}>
             Change
           </Button>
         </div>
       </div>
-      <Button onClick={handleSubmit(saveData)}>Update</Button>
+      <Button onClick={handleSubmit(saveData)} disabled={loading}>
+        Update
+      </Button>
     </form>
   );
 };
