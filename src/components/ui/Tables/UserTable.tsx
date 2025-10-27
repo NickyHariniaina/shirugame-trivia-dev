3709093ebde25150240b04def35739a7395f577b
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-27 17:29:55 +0300

    feat: add redicection to the targeted user profile.

--- src/components/ui/Tables/UserTable.tsx
+++ src/components/ui/Tables/UserTable.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { User } from "@/types/db";
 import { Avatar } from "../Avatar";
 import {  TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "../table";
@@ -7,6 +9,7 @@ type UserTablePropsType = {
 };
 
 export const UserTable = ({ users }: UserTablePropsType) => {
+  const router = useRouter();
   return (
     <div className="shadow-2xl dark:shadow-blue-700 max-w-full overflow-x-hidden border rounded-lg">
       <Table className="min-w-[400px] md:min-w-[600px] lg:min-w-[800px]">
@@ -19,7 +22,7 @@ export const UserTable = ({ users }: UserTablePropsType) => {
         </TableHeader>
         <TableBody>
           {users.map((user) => (
-            <TableRow key={user.id}>
+            <TableRow key={user.id} onClick={() => router.push(`/user/${user.username}`)}>
               <TableCell>{user.rank}</TableCell>
               <TableCell className="flex flex-row gap-2 items-center"><Avatar src={user.image || ""} alt={user.username || ""} size={20} /><span className="text-sm">{user.username}</span></TableCell>
               <TableCell>{user.highestScore}</TableCell>
