7ff6b520b664f117860e49dcfbf9dd55a76daa9f
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-27 17:23:21 +0300

    feat: add leaderboard table.

--- src/components/ui/Leaderboard/LeaderboardBody.tsx
+++ src/components/ui/Leaderboard/LeaderboardBody.tsx
@@ -2,6 +2,8 @@
 import { getUsersWithCriteria } from "@/utils/func";
 import { User } from "@/types/db";
 import { useState, useEffect } from "react";
+import { Spinner } from "../spinner";
+import { UserTable } from "../Tables/UserTable";
 
 export const LeaderboardBody = () => {
   const [users, setUsers] = useState<User[]>([]);
@@ -16,8 +18,7 @@ export const LeaderboardBody = () => {
   }, []);
 
   console.log(users);
-
-  return <div className="flex flex-col gap-5">
-
+  return <div className="flex flex-col gap-5 items-center justify-center m-4">
+    {users.length === 0 ? <Spinner /> : <UserTable users={users} />}
   </div>;
 };
