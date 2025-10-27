2f7e1afc2ffd3ea144e06933e3cc3743859d3eeb
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-27 16:40:42 +0300

    feat: relpace basic getUsers route with a more advanced one

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,35 +1,28 @@
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export const GET = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+export const GET = async (req: NextRequest) => {
+  const session = await auth.api.getSession({ headers: await headers() });
 
   if (!session) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
   try {
-    const users = await prisma.user.findMany();
+    const criteria = req.nextUrl.searchParams.get("criteria") || "createdAt";
 
-    return NextResponse.json(
-      {
-        data: users,
-      },
-      { status: 200 },
-    );
+    const allowedFields = ["username", "email", "createdAt", "rank", "highestScore"];
+    const sortBy = allowedFields.includes(criteria) ? criteria : "createdAt";
+
+    const users = await prisma.user.findMany({
+      orderBy: { [sortBy]: "asc" },
+    });
+
+    return NextResponse.json({ data: users }, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      {
-        message: "Internal Server Error",
-      },
-      {
-        status: 500,
-      },
-    );
+    console.error(error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
   }
 };
