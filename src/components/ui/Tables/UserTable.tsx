0ab0dda4f4c62211657e3a720ab51018917e3d94
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-27 17:28:01 +0300

    style: add pp to leaderboard

--- src/components/ui/Tables/UserTable.tsx
+++ src/components/ui/Tables/UserTable.tsx
@@ -1,4 +1,5 @@
 import { User } from "@/types/db";
+import { Avatar } from "../Avatar";
 import {  TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "../table";
 
 type UserTablePropsType = {
@@ -7,7 +8,7 @@ type UserTablePropsType = {
 
 export const UserTable = ({ users }: UserTablePropsType) => {
   return (
-    <div className="shadow-2xl dark:shadow-blue-900 max-w-full overflow-x-hidden border rounded-lg">
+    <div className="shadow-2xl dark:shadow-blue-700 max-w-full overflow-x-hidden border rounded-lg">
       <Table className="min-w-[400px] md:min-w-[600px] lg:min-w-[800px]">
         <TableHeader>
           <TableRow>
@@ -20,7 +21,7 @@ export const UserTable = ({ users }: UserTablePropsType) => {
           {users.map((user) => (
             <TableRow key={user.id}>
               <TableCell>{user.rank}</TableCell>
-              <TableCell>{user.username}</TableCell>
+              <TableCell className="flex flex-row gap-2 items-center"><Avatar src={user.image || ""} alt={user.username || ""} size={20} /><span className="text-sm">{user.username}</span></TableCell>
               <TableCell>{user.highestScore}</TableCell>
             </TableRow>
           ))}
