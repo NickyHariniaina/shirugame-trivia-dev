d0505c3be80cb993127d023738d0d20d467a9641
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-28 12:51:20 +0300

    feat: add userId page.

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,9 +2,14 @@
 
 import { useParams } from "next/navigation";
 
+// I accidentally put username instead of id.
 const Page = () => {
   const params = useParams();
-  return <div>User</div>
+  const userId = params.username;
+  console.log(userId);
+  return <div>
+
+  </div>
 }
 
 export default Page
