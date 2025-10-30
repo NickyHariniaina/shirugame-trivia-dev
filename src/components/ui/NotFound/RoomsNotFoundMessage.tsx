ec3b4711b80c024dd4a2868aaa7e1461bb191087
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-30 12:19:55 +0300

    feat: add fallback message when no rooms

---
+++ src/components/ui/NotFound/RoomsNotFoundMessage.tsx
@@ -0,0 +1,7 @@
+export const RoomsNotFoundMessage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p>No rooms yet... come back later</p>
+    </div>
+  );
+};
--- src/components/ui/Room/GameList.tsx
+++ src/components/ui/Room/GameList.tsx
@@ -1,61 +1,55 @@
-'use client'
+"use client";
 import { Button } from "../button";
 import { ScrollArea } from "../scroll-area";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RoomSearchBar } from "../SearchBar/RoomSearchBar";
 import { Spinner } from "../spinner";
+import { Room } from "@/types/db";
 
 export const GameList = () => {
-  // Mock.
-  const games = [
-    {
-      name: "Chess",
-      host: "Nicky",
-    },
-    {
-      name: "Chess",
-      host: "Nicky",
-    },
-    {
-      name: "Checkers",
-      host: "John",
-    },
-    {
-      name: "Go",
-      host: "Jane",
-    },
-    {
-      name: "Dots and Boxes",
-      host: "John",
-    },
-    {
-      name: "Tic Tac Toe",
-      host: "Jane",
-    },
-  ];
+  const [rooms, setRooms] = useState<Room[]>([]);
+
   const [searchedRooms, setSearchedRooms] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
+  const fetchRooms = async () => {
+    setLoading(true);
+    const response = await fetch("/api/rooms");
+    const data = await response.json();
+    setRooms(data.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
   return (
     <div className="flex flex-col gap-2">
-      <h3>List of avalaible game:</h3>
+      <h3>List of avalaible room:</h3>
       <RoomSearchBar loading={loading} setSearchedRooms={setSearchedRooms} />
       <ScrollArea className="flex flex-col gap-3 rounded-md border p-4 h-100">
         {!loading ? (
-          games.map((game, index) => {
-            return (
-              <li
-                key={index}
-                className="border-y p-3 flex gap-4 items-center justify-between"
-              >
-                <div>
-                  <p className="text-2xl">{game.name}</p>
-                  <p>{game.host}</p>
-                </div>
-                <Button>Join</Button>
-              </li>
-            );
-          })
+          rooms.length === 0 ? (
+            <div className="flex flex-col items-center justify-center">
+              <p>No rooms yet... come back later</p>
+            </div>
+          ) : (
+            rooms.map((room, index) => {
+              return (
+                <li
+                  key={index}
+                  className="border-y p-3 flex gap-4 items-center justify-between"
+                >
+                  <div>
+                    <p className="text-2xl">{room.title}</p>
+                    <p>{room.openedBy.email}</p>
+                  </div>
+                  <Button>Join</Button>
+                </li>
+              );
+            })
+          )
         ) : (
           <Spinner />
         )}
