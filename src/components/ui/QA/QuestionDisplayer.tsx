35932ed5d118655d92b082cab34f513b9cc416f7
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-31 10:52:34 +0300

    feat: add question displayer component with empty value for qst

---
+++ src/components/ui/QA/QuestionDisplayer.tsx
@@ -0,0 +1,23 @@
+import { Question } from "@/types/db";
+import { ScrollArea } from "../scroll-area";
+
+type QuestionDisplayerProps = {
+  questions: Question[];
+};
+
+export const QuestionDisplayer = (props:  QuestionDisplayerProps) => {
+
+  const questions: string[] = props.questions.map((question: Question) => {
+    return question.question;
+  });
+
+  return <ScrollArea className="flex flex-col w-[75%] gap-3 rounded-md border p-4 h-40">
+    {
+      questions.map((question, index) => {
+        return <div key={index} className="flex flex-col gap-2">
+          <p className='py-4 border-y'>{}</p>
+        </div>
+      })
+    }
+  </ScrollArea>
+}
--- src/components/ui/Room/RoomCreationBodyCard.tsx
+++ src/components/ui/Room/RoomCreationBodyCard.tsx
@@ -1,14 +1,40 @@
-import { DateAndHourPicker } from "../DateAndHourPicker"
-import { Input } from "../input"
+"use client";
+import { useForm } from "react-hook-form";
+import { Input } from "../input";
+import { User } from "@/types/db";
+import { AddAndRemoveButton } from "../Button/AddAndRemoveButton";
+import { useState } from "react";
+import { Button } from "../button";
+import { QuestionDisplayer } from "../QA/QuestionDisplayer";
+type FormValues = {
+  title: string;
+  players: User[];
+  numberOfQuestion: number;
+};
 
 export const RoomCreationBodyCard = () => {
-  return <div className='flex flex-col gap-4'>
-    <div className='flex flex-col gap-4'>
-      <div className='flex flex-col gap-2'>
-        <Input placeholder='Choose a title for your room' type='text'/>
-        <Input placeholder='Choose a description for your room' type='text'/>
-        <DateAndHourPicker/>
+  const [numberOfQuestion, setNumberOfQuestion] = useState(1);
+  const { register } = useForm<FormValues>();
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col items-center gap-4">
+          <Input
+            placeholder="Choose a title"
+            type="text"
+            {...register("title")}
+          />
+          <div className="flex flex-col items-center justify-center gap-4">
+            <p>Number of players</p>
+            <div className="flex flex-row items-center gap-4">
+              <div>{numberOfQuestion}</div>
+              <AddAndRemoveButton setNumberOfQuestion={setNumberOfQuestion} />
+            </div>
+            <Button variant="secondary">Generate questions</Button>
+            <QuestionDisplayer numberOfQuestion={numberOfQuestion} />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
