d4d7bf2b6857f081a8d93439573ec776395db865
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-31 17:10:27 +0300

    feat: add creat room route api

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { createRoom } from "@/services/room";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
 export const GET = async () => {
   try {
@@ -7,6 +10,25 @@ export const GET = async () => {
 
     return NextResponse.json({data: rooms});
 
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+export const POST = async (req: Request) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const data = await req.json();
+    const userId = session.user.id;
+    await createRoom(data, userId);
+    return NextResponse.json({ message: "Room created successfully" }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
