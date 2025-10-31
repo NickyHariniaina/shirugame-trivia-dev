5e776e867a87413a63ad90b75b8227fbaedcf72c
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-31 17:08:28 +0300

    feat: add create room service.

--- src/services/room.ts
+++ src/services/room.ts
@@ -1,6 +1,23 @@
+import { prisma } from "@/lib/prisma";
+import { Question } from "@/types/db";
 import { RoomDto } from "@/types/dto";
 
-export const createRoom = async (data: RoomDto) => {
-  :w
+export const createRoom = async (data: RoomDto, userId: string) => {
+  const questionsId: {id: string}[] = data.questions.map((question: Question) => {
+    return { id: question.id };
+  });
 
+  await prisma.room.create({
+    data: {
+      id: data.title,
+      title: data.title,
+      questions: {
+        connect: questionsId,
+      },
+      openedById: userId,
+      players: {
+        connect: [{ id: userId }],
+      }
+    }
+  })
 }
