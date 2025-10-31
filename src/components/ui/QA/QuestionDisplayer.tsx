b4f463d88a23c5268c9077b52f7493ddc33aca83
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-31 15:39:21 +0300

    feat: add fallback for empty questions

--- src/components/ui/QA/QuestionDisplayer.tsx
+++ src/components/ui/QA/QuestionDisplayer.tsx
@@ -13,20 +13,20 @@ export const QuestionDisplayer = (props: QuestionDisplayerProps) => {
   });
   // TODO: Add fallback for empty questions
   return (
-<ScrollArea className="w-full h-40 rounded-md border p-4">
-  {props.loading ? (
-    <div className="flex h-full w-full items-center justify-center">
-      <Spinner />
-    </div>
-  ) : (
-    <div className="flex flex-col items-center gap-3">
-      {questions.map((question, index) => (
-        <div key={index} className="flex flex-col gap-2 w-full">
-          <p className="py-4 border-y">{question}</p>
+    <ScrollArea className="w-full h-40 rounded-md border p-4">
+      {props.loading ? (
+        <div className="flex h-full w-full items-center justify-center">
+          <Spinner />
         </div>
-      ))}
-    </div>
-  )}
-</ScrollArea>
+      ) : questions.length === 0 ? <p>No questions found</p> : (
+        <div className="flex flex-col items-center gap-3">
+          {questions.map((question, index) => (
+            <div key={index} className="flex flex-col gap-2 w-full">
+              <p className="py-4 border-y">{question}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </ScrollArea>
   );
 };
