4986ebc89087457e35f388094aee442675bc75d4
NickyHariniaina <nickyharini1na@gmail.com>
2025-11-01 17:44:49 +0300

    style: Add header to the room RoomDisplayer page.

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,9 @@
 import { RoomDisplayer } from "@/components/ui/Room/RoomDisplayer";
+import { RoomHeader } from "@/components/ui/Room/RoomHeader";
 
 const Page = () => {
   return <div className='flex flex-col justify-center items-center gap-3 p-3 m-3'>
+    <RoomHeader />
     <RoomDisplayer />
   </div>
 }
--- src/components/ui/Room/RoomDisplayer.tsx
+++ src/components/ui/Room/RoomDisplayer.tsx
@@ -1,14 +1,22 @@
 "use client"
+import { authClient } from "@/lib/auth-client"
 import { Room } from "@/types/db"
 import { fetchRoomById } from "@/utils/func"
 import { useParams } from "next/navigation"
 import { useEffect, useState } from "react"
 
-
 export const RoomDisplayer = () => {
   const [room, setRoom] = useState<Room>()
   const params = useParams()
   const roomId = params.roomId as string
+  const {data: session} = authClient.useSession();
+  const [isOwner, setIsOwner] = useState<boolean>();
+
+  if (session?.user.id == room?.openedBy.id) {
+    setIsOwner(true);
+  } else {
+    setIsOwner(false);
+  }
 
   const fetchRoom = async () => {
     const fetchedRoom: Room = await fetchRoomById(roomId)
