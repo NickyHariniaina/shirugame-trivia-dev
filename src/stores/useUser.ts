273586bf6b9140ebe5314c9eb236000a69e5d166
NickyHariniaina <nickyharini1na@gmail.com>
2025-12-29 13:15:45 +0300

    chore: add loading element in the use store

--- src/stores/useUser.ts
+++ src/stores/useUser.ts
@@ -4,12 +4,16 @@ import { fetchMissingField } from "@/utils/func";
 import { create } from "zustand";
 
 type UserStore = {
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
   userData: User | null;
   setUserData: (userData: UserDto | undefined) => void;
 };
 
 export const useUser = create<UserStore>((set) => ({
   userData: null,
+  loading: false,
+  setLoading: (loading: boolean) => set({ loading }),
   setUserData: async (userData: UserDto | undefined) => {
     if (!userData) throw new Error("No user data");
 
