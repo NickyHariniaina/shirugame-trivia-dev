6686c9af85d72355090d1985aad2e4d1fa0395e9
NickyHariniaina <nickyharini1na@gmail.com>
2025-12-29 13:37:17 +0300

    feat: add loading on OAuth button

--- src/components/ui/Auth/OAuth.tsx
+++ src/components/ui/Auth/OAuth.tsx
@@ -3,20 +3,22 @@ import { Button } from "../button";
 import { authClient } from "@/lib/auth-client";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
-    import toast from "react-hot-toast";
+import toast from "react-hot-toast";
 import { useUser } from "@/stores/useUser";
+import { Spinner } from "../spinner";
 
 export const OAuth = () => {
-  const { setLoading } = useUser();
+  const setLoading = useUser((state) => state.setLoading);
+  const loading = useUser((state) => state.loading);
   const handleGoogleSign = async () => {
-    setLoading(true);
+    console.log(loading)
     const data = await authClient.signIn.social({
       provider: "google",
     });
     if (data) {
       toast.success("Redirecting to Google...");
-      setLoading(false);
     }
+    setLoading(false);
   };
 
   const handleGithubSign = async () => {
@@ -26,9 +28,10 @@ export const OAuth = () => {
     });
     if (data) {
       toast.success("Redirecting to GitHub...");
-      setLoading(false);
     }
+    setLoading(false);
   };
+
   return (
     <div className="flex flex-col md:flex-row justify-center items-center gap-5 md:justify-around">
       <Button
@@ -37,7 +40,7 @@ export const OAuth = () => {
                    dark:bg-gray-100 dark:text-gray-900 dark:hover:bg-gray-200"
         onClick={handleGoogleSign}
       >
-        <FcGoogle /> Continue with Google
+        { loading ? ( <Spinner /> ) : ( <FcGoogle /> )} Continue with Google
       </Button>
       <Button
         className="flex items-center gap-2
