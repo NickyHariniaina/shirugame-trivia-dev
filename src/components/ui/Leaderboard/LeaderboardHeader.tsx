40872a678b9621af6581678c90291da78b1c9b0b
NickyHariniaina <nickyharini1na@gmail.com>
2026-01-01 22:52:18 +0300

    fix: move mode toogle outside the back button

--- src/components/ui/Leaderboard/LeaderboardHeader.tsx
+++ src/components/ui/Leaderboard/LeaderboardHeader.tsx
@@ -10,8 +10,8 @@ export const LeaderboardHeader = () => {
     <div className="m-4 md:m-5 flex flex-row items-center justify-center gap-3 md:gap-5">
       <Button onClick={() => router.push("/")} variant="ghost" className="p-2 md:p-3">
         <StepBack className="w-5 h-5 md:w-6 md:h-6" />
-        <ModeToggle />
       </Button>
+      <ModeToggle />
       <h1 className="text-3xl sm:text-4xl md:text-6xl text-center">
         Shirugame
       </h1>
