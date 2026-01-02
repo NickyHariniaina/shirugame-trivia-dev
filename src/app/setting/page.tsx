f7f4184c2e4fd94ea4c21a8c023fc1ceb31876d1
NickyHariniaina <nickyharini1na@gmail.com>
2026-01-02 13:05:34 +0300

    feat: add header to setting page

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,9 +1,23 @@
 'use client'
-
+import { Header } from "@/components/ui/Header";
 import { authClient } from "@/lib/auth-client";
+import { useEffect, useState } from "react";
 
 const Page = () => {
-  return <div>Profil</div>
+  const { data: session } = authClient.useSession();
+  const [logged, setLogged] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (session?.user) {
+      setLogged(true);
+    } else {
+      setLogged(false);
+    }
+  }, [session]);
+
+  return <div>
+    <Header logged={logged}/>
+  </div>
 }
 
 export default Page
