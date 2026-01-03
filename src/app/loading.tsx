2403668212dda999695c52834a80b69dc5b0721d
NickyHariniaina <nickyharini1na@gmail.com>
2026-01-03 18:57:07 +0300

    fix

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/spinner";
 
 const Page = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black dark:bg-white bg-opacity-50 z-50">
       <div className="flex flex-row justify-center items-center gap-4">
         <Spinner />
         <p>
