cc23e16694bdf2cba5d838f1c93213fd95497a39
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-06 17:04:44 +0300

    chore: refetch session list after revoking one session

--- src/components/ui/setting/session-setting.tsx
+++ src/components/ui/setting/session-setting.tsx
@@ -2,12 +2,13 @@
 import { authClient } from "@/lib/auth-client"
 import { Session } from "@/types/better-auth"
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { Button } from "../shadcn-component/button";
 
 
 export function SessionSetting() {
 	const [sessions, setSessions] = useState<Session["session"][] | null>(null);
-	useEffect(() => {
-		const fetchSessions = async () => {
+	const fetchSessions = async () => {
 			try {
 				const sessions = await authClient.listSessions();
 				console.log("Fetched sessions:", sessions.data);
@@ -16,9 +17,23 @@ export function SessionSetting() {
 				console.error("Error fetching sessions:", error);
 			}
 		}
+
+	useEffect(() => {
 		fetchSessions();
 	}, []);
 
+	const revokeSession = async (sessionId: string) => {
+		try {
+			await authClient.revokeSession({
+				token: sessionId
+			});
+			toast.success("Session ended successfully");
+			fetchSessions(); // Refresh the session list after revoking
+		} catch (error) {
+			console.error("Error revoking session:", error);
+			toast.error("Failed to end session");
+		}
+	}
   return (
     <div className="flex flex-col gap-4">
 			<h2 className="text-2xl font-bold">Session</h2>
@@ -26,8 +41,9 @@ export function SessionSetting() {
 			{
 				sessions ? (
 					sessions.map((session:Session["session"]) => (
-						<li key={session.id} className="p-4 border rounded-md">
+						<li key={session.id} className="flex flex-col gap-3 p-4 border rounded-md">
 							<div>Connected on <span className="italic">{session.userAgent}</span> <br/><br/> Created at: <span className="italic">{session.createdAt.toLocaleString()}</span></div>
+							<Button variant="destructive" onClick={() => revokeSession(session.token)} className="self-end">End session</Button>
 						</li>
 					))
 				): null
