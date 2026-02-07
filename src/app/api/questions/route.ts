425df9241aac72ef148bad606c9b14db049da3df
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-07 18:10:52 +0300

    chore: add if section for countPage logic

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -3,29 +3,37 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
+    const numberOfQuestionQueryNotFormatted =
+      req?.nextUrl?.searchParams?.get("numberOfQuestion") || "null";
 
     // TODO: Add randomized parameters later
-    const numberOfQuestionQueryNotFormatted=  req?.nextUrl?.searchParams?.get("numberOfQuestion") || "null";
-    
+
     let questions;
 
-    if (numberOfQuestionQueryNotFormatted === "null" ) {
+    if (numberOfQuestionQueryNotFormatted === "null") {
       const limit = req?.nextUrl?.searchParams?.get("limit") || "10";
       const page = req?.nextUrl?.searchParams?.get("page") || "1";
+
+      if (req?.nextUrl?.searchParams?.get("countPage") == "true") {
+        const totalQuestions = await prisma.question.count();
+        const maxPageNumber = totalQuestions / parseInt(limit)
+        return NextResponse.json({ data: maxPageNumber }, { status: 200 });
+      }
+
       const offset = (parseInt(page) - 1) * parseInt(limit);
       questions = await prisma.$queryRaw`
         SELECT * FROM "public"."Question"
         ORDER BY "typeId"
         LIMIT ${parseInt(limit)}
         OFFSET ${offset}
-      `
+      `;
     } else {
       const numberOfQuestion = parseInt(numberOfQuestionQueryNotFormatted);
       questions = await prisma.$queryRaw`
         SELECT * FROM "public"."Question"
         ORDER BY RANDOM()
         LIMIT ${numberOfQuestion}
-      `
+      `;
     }
     return NextResponse.json({ data: questions }, { status: 200 });
   } catch (error) {
