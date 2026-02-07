2fbb1c4b2a15c8ad3187091af44b6e9f30cfb813
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-07 17:35:29 +0300

    fix: import QuestionsTable on page.tsx

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,7 +1,9 @@
+import { QuestionsTable } from "@/components/ui/tables/question-table";
+
 const Page = () => {
     return <div>
         <h2 className="text-2xl font-bold">Shirugame&apos;s questions</h2>
-        <QuestionTable />
+        <QuestionsTable />
     </div>
 }
 
