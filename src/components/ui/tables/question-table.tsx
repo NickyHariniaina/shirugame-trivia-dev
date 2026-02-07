24a524cb6cd4cbd795d16e2e4c4d3297a6a25b54
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-07 17:34:32 +0300

    feat: displey 10 questions by default

--- src/components/ui/tables/question-table.tsx
+++ src/components/ui/tables/question-table.tsx
@@ -2,36 +2,40 @@ import { Table } from "lucide-react"
 import router from "next/router"
 import { Avatar } from "../profil/avatar"
 import { TableHeader, TableRow, TableHead, TableBody, TableCell } from "../shadcn-component/table"
+import { useEffect, useState } from "react"
+import { getAllQuestions } from "@/utils/func"
+import { Question } from "@/types/db"
 
 export const QuestionsTable = () => {
 
-    
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            const questions = await getAllQuestions(1, 10);
+            setQuestions(questions);
+        }
+        fetchQuestions();
+    }, [])
 
     return <div className="shadow-2xl dark:shadow-blue-700 max-w-full overflow-x-hidden border rounded-lg">
       <Table className="min-w-[400px] md:min-w-[600px] lg:min-w-[800px]">
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[5%] text-left">Rank</TableHead>
-            <TableHead className="text-left w-[20%]">Username</TableHead>
-            <TableHead className="text-left w-[10%]">Score</TableHead>
+            <TableHead className="w-[5%] text-left">Type</TableHead>
+            <TableHead className="text-left w-[20%]">Question</TableHead>
+            <TableHead className="text-left w-[10%]">Answer</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {users.map((user) => (
+          {questions.map((question: Question) => (
             <TableRow
-              key={user.id}
-              onClick={() => router.push(`/user/${user.id}`)}
+              key={question.id}
+              onClick={() => router.push(`/question/${question.id}`)}
             >
-              <TableCell>{user.rank}</TableCell>
-              <TableCell className="flex flex-row gap-2 items-center">
-                <Avatar
-                  src={user.image || ""}
-                  alt={user.username || ""}
-                  size={20}
-                />
-                <span className="text-sm">{user.username || user.email}</span>
-              </TableCell>
-              <TableCell>{user.highestScore}</TableCell>
+              <TableCell>{question.type.name}</TableCell>
+              <TableCell>{question.question}</TableCell>
+              <TableCell>{question.Answer}</TableCell>
             </TableRow>
           ))}
         </TableBody>
