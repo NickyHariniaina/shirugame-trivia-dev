d53f671002578e271b182fa5c369c5f067da7d10
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-09 15:43:34 +0300

    chore: add react-hook-form for rquestio

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import FullScreenLoader from "@/components/ui/loading/fullscreen";
 import { FilterSelector } from "@/components/ui/room/filter-selector";
+import { useForm } from "react-hook-form";
 import { QuestionSearchBar } from "@/components/ui/search-bar/question-search-bar";
 import { Button } from "@/components/ui/shadcn-component/button";
 import { Textarea } from "@/components/ui/shadcn-component/textarea";
@@ -11,18 +12,42 @@ import { getAllQuestions, getMaxPage } from "@/utils/func";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
+type QuestionForm = {
+  question: string
+  answer: string
+}
+
 const Page = () => {
     const { data: session, isPending } = authClient.useSession();
     const [showSubmitQuestion, setShowSubmitQuestion] = useState(false);
     const [maxPage, setMaxPage] = useState<number>(1);
     const [page, setPage] = useState(1);
-    const [question, setQuestion] = useState("");
     const [loading, setLoading] = useState<boolean>(false);
     const [dispayledQuestions, setDispayledQuestions] = useState<Question[]>([]);
-    const [answer, setAnswer] = useState("");
     const [typeId, setTypeId] = useState<string>("null");
     const [searchValue, setSearchValue] = useState<string>("");
-
+    const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { isSubmitting },
+    } = useForm<QuestionForm>();
+    const onSubmitQuestion = async (data: QuestionForm) => {
+      if (!session) {
+        toast.error("You need to log in to submit a question.");
+        return;
+      }
+      try {
+        setLoading(true);
+        toast.success("Question sent to the admin!, check your notifications for updates!");
+        reset();
+        setShowSubmitQuestion(false);
+      } catch (e) {
+        toast.error("Failed to submit question");
+      } finally {
+        setLoading(false);
+      }
+    };
       useEffect(() => {
         const fetchQuestions = async () => {
             setLoading(true)
@@ -71,19 +96,39 @@ const Page = () => {
 
     return <div className="flex flex-col gap-4 m-4 p-3 justify-center items-center">
         <h2 className="text-2xl font-bold">Shirugame&apos;s questions</h2>
-        <Button variant="outline" onClick={handleGoSubmitButton} disabled>Go submit your own question</Button>
+        <Button variant="outline" onClick={handleGoSubmitButton}>Go submit your own question</Button>
+        {showSubmitQuestion && (
+          <form
+            onSubmit={handleSubmit(onSubmitQuestion)}
+            className="flex flex-col gap-2 p-5 border rounded-lg w-full md:w-[600px]"
+          >
+            <h3 className="text-xl font-semibold">Submit your question</h3>
+
+            <Textarea
+              placeholder="Your question"
+              {...register("question", { required: true, minLength: 5 })}
+            />
+
+            <Textarea
+              placeholder="The answer of your question"
+              {...register("answer", { required: true, minLength: 2 })}
+            />
+
+            <p className="text-sm text-muted-foreground">
+              Your questions will be sent to the Admin of Shirugame.
+            </p>
+
+            <Button type="submit" variant="outline" disabled={isSubmitting}>
+              Submit
+            </Button>
+
+            <Button type="button" variant="ghost" onClick={handleGoSubmitButton}>
+              Cancel
+            </Button>
+          </form>
+        )}
         <FilterSelector setTypeId={setTypeId} typeId={typeId} />
         <QuestionSearchBar searchValue={searchValue} setSearchValue={setSearchValue} handleSearch={handleSearch} handleClearValue={handleClearValue} />
-        {
-            showSubmitQuestion && <div className="flex flex-col gap-2 p-5 border rounded-lg  w-full md:w-[600px]">
-                <h3 className="text-xl font-semibold">Submit your question</h3>
-                <Textarea placeholder="Your question" value={question} onChange={(e) => setQuestion(e.target.value)} />
-                <Textarea placeholder="The answer of your question" value={answer} onChange={(e) => setAnswer(e.target.value)} />
-                <p className="text-sm text-muted-foreground">Your questions will be sent to the Admin of Shirugame. Check your notifications for updates!</p>
-                <Button variant="outline">Submit</Button>
-                <Button variant="ghost" onClick={handleGoSubmitButton}>Cancel</Button>
-            </div>
-        }
         <QuestionsTable questions={dispayledQuestions} setQuestions={setDispayledQuestions} loading={loading} setLoading={setLoading} maxPage={maxPage} setMaxPage={setMaxPage}  page={page} setPage={setPage} />
     </div>
 }
