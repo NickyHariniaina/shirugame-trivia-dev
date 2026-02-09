9883ccd3ee6e8dc3a9365247d1d6caa90e584bdb
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-09 11:57:24 +0300

    chore: create route query for search

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -6,13 +6,24 @@ export const GET = async (req: NextRequest) => {
     const numberOfQuestionQueryNotFormatted =
       req?.nextUrl?.searchParams?.get("numberOfQuestion") || "null";
 
-    // TODO: Add randomized parameters later
-
     let questions;
 
     if (numberOfQuestionQueryNotFormatted === "null") {
       const limit = req?.nextUrl?.searchParams?.get("limit") || "10";
       const page = req?.nextUrl?.searchParams?.get("page") || "1";
+
+        if (req?.nextUrl?.searchParams?.get("search") != null) {
+            const search = req?.nextUrl?.searchParams?.get("search") || "null";
+            questions = await prisma.question.findMany({
+                where: {
+                    question: {
+                        contains: search,
+                        mode: "insensitive",
+                    },
+                }
+            });
+            return NextResponse.json({ data: questions }, { status: 200 });
+        }
 
       if (req?.nextUrl?.searchParams?.get("countPage") == "true") {
         const totalQuestions = await prisma.question.count();
