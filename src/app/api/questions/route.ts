74d41c2c9184cb77f2901577bea92ee57a5156fe
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-10 19:18:27 +0300

    chore: only fetched valid questions

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -2,68 +2,92 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-  try {
-    const numberOfQuestionQueryNotFormatted =
-      req?.nextUrl?.searchParams?.get("numberOfQuestion") || "null";
+    try {
+        const numberOfQuestionQueryNotFormatted =
+            req?.nextUrl?.searchParams?.get("numberOfQuestion") || "null";
 
-    let questions;
+        let questions;
 
-    if (numberOfQuestionQueryNotFormatted === "null") {
-        if (req?.nextUrl?.searchParams?.get("search") != null) {
-            if (req?.nextUrl?.searchParams?.get("typeId") != null) {
+        if (numberOfQuestionQueryNotFormatted === "null") {
+            if (req?.nextUrl?.searchParams?.get("search") != null) {
+                if (req?.nextUrl?.searchParams?.get("typeId") != null) {
+                    const search = req?.nextUrl?.searchParams?.get("search");
+                    const typeId = req?.nextUrl?.searchParams?.get("typeId");
+                    questions = await prisma.question.findMany({
+                        where:
+                            search && typeId
+                                ? {
+                                      question: {
+                                          contains: search,
+                                          mode: "insensitive",
+                                      },
+                                      typeId: typeId,
+                                      isValidated: true,
+                                  }
+                                : undefined,
+                        orderBy: { typeId: "asc" },
+                    });
+                    return NextResponse.json(
+                        { data: questions },
+                        { status: 200 },
+                    );
+                }
                 const search = req?.nextUrl?.searchParams?.get("search");
-                const typeId = req?.nextUrl?.searchParams?.get("typeId");
                 questions = await prisma.question.findMany({
-                    where: search && typeId ?
-                        { question: { contains: search, mode: "insensitive" }, typeId: typeId }
+                    where: search
+                        ? {
+                              question: {
+                                  contains: search,
+                                  mode: "insensitive",
+                              },
+                              isValidated: true,
+                          }
                         : undefined,
                     orderBy: { typeId: "asc" },
                 });
                 return NextResponse.json({ data: questions }, { status: 200 });
+            } else {
+                if (req?.nextUrl?.searchParams?.get("typeId") != null) {
+                    const typeId = req?.nextUrl?.searchParams?.get("typeId");
+                    questions = await prisma.question.findMany({
+                        where: typeId
+                            ? { typeId: typeId, isValidated: true }
+                            : undefined,
+                        orderBy: { typeId: "asc" },
+                    });
+                    return NextResponse.json(
+                        { data: questions },
+                        { status: 200 },
+                    );
+                }
             }
-            const search = req?.nextUrl?.searchParams?.get("search");
-                questions = await prisma.question.findMany({
-                  where: search
-                    ? { question: { contains: search, mode: "insensitive" } }
-                    : undefined,
-                  orderBy: { typeId: "asc" },
-                });
-            return NextResponse.json({ data: questions }, { status: 200 });
-        } else {
-            if (req?.nextUrl?.searchParams?.get("typeId") != null) {
-                const typeId = req?.nextUrl?.searchParams?.get("typeId");
-                questions = await prisma.question.findMany({
-                    where: typeId ?
-                        { typeId: typeId }
-                        : undefined,
-                    orderBy: { typeId: "asc" },
-                });
-                return NextResponse.json({ data: questions }, { status: 200 });
-            }
-        }
 
-        questions = await prisma.question.findMany({
-          orderBy: { typeId: "asc" },
-        });
-    } else {
-      const numberOfQuestion = parseInt(numberOfQuestionQueryNotFormatted);
-      questions = await prisma.$queryRaw`
+            questions = await prisma.question.findMany({
+                where: { isValidated: true },
+                orderBy: { typeId: "asc" },
+            });
+        } else {
+            const numberOfQuestion = parseInt(
+                numberOfQuestionQueryNotFormatted,
+            );
+            questions = await prisma.$queryRaw`
         SELECT * FROM "public"."Question"
+        WHERE "isValidated" = true
         ORDER BY RANDOM()
         LIMIT ${numberOfQuestion}
       `;
+        }
+        return NextResponse.json({ data: questions }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error:
+                    "Something went wrong while fetching questions, please try again later " +
+                    error,
+            },
+            { status: 500 },
+        );
     }
-    return NextResponse.json({ data: questions }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      {
-        error:
-          "Something went wrong while fetching questions, please try again later " +
-          error,
-      },
-      { status: 500 },
-    );
-  }
 };
 
 export const POST = async (req: NextRequest) => {
