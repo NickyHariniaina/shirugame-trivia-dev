25ca009d128de93b1686e9a81f40808b5d6d206d
NickyHariniaina <nickyharini1na@gmail.com>
2026-02-13 18:35:04 +0300

    chore: add route for notification update

---
+++ src/app/api/notifications/[id]/route.ts
@@ -0,0 +1,53 @@
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PATCH = async (
+    req: NextRequest,
+    context: RouteContext<"/api/notifications/[id]">,
+) => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        const { id: notificationId } = await context.params;
+        const dataReceived = await req.json();
+
+        // TODO: Update in case you need to add other field later
+        const allowedFields = ["seen"];
+
+        if (!allowedFields.includes(dataReceived.field)) {
+            return NextResponse.json(
+                {
+                    message: "Invalid field",
+                },
+                {
+                    status: 400,
+                },
+            );
+        }
+
+        await prisma.notification.update({
+            where: {
+                id: notificationId,
+            },
+            data: dataReceived,
+        });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {
+                message: "Internal Server Error",
+            },
+            {
+                status: 500,
+            },
+        );
+    }
+};
